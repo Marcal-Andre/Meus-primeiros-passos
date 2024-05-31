35bb37713dedd3969b041833af68b1a95c71dfd3
Marcal-Andre <155629428+Marcal-Andre@users.noreply.github.com>
2024-05-30 23:55:41 -0300

    Sistema-de-Cadastro

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            font-family: arial, Helvetica, sans-serif;
+        }
+    
+    </style>
+</head>
+<body>
+    <h1>Se Escreva no Canal</h1>
+    <h2>André Marçal</h2>
+    <div href="box">
+        <a href=" ">Login</a>
+        <a href=" ">Cadastre-se</a>
+
+    </div>
+    
+</body>
+</html>
